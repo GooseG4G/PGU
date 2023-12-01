13bfee77e8f8c3a9e8780cecbc44ebbde2628e64
Goose <113329604+GooseG4G@users.noreply.github.com>
2023-12-01 12:29:24 +0300

    Create file_generator.py

---
+++ Hardware components of automated systems/LAB4 (Threading and Multiprocessing)/file_generator.py
@@ -0,0 +1,18 @@
+import os
+from string import ascii_letters as letters
+from string import digits
+from random import choice
+
+ABC = f'{letters}{digits}'
+SIZE_TABLE = {'B': 1, 'KB': 1024, 'MB': 1024 * 1024}
+
+
+def fill(size=1, suff='MB', encoding='utf-8'):
+    fileName = ''.join(choice(ABC) for n in range(8))
+    writeString = bytes('', encoding=encoding)
+    while len(writeString) < SIZE_TABLE[suff] / size:
+        writeString += bytes(choice(ABC), encoding=encoding)
+    tempFile = open(fileName, 'wb+')
+    tempFile.write(writeString)
+    tempFile.close()
+    os.remove(os.path.join(os.getcwd(), fileName))
